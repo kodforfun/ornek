683f20ba9b0189c397c5517a42c275f14a6bc33d
kodforfun <64761654+kodforfun@users.noreply.github.com>
2020-11-08 21:07:22 +0300

    Update hello.c

--- hello.c
+++ hello.c
@@ -14,24 +14,54 @@ static void intyazdir(const char *s, const int a) {
   snprintf(out,32,s,a);
   hal_send_str(out);
 }
+int *bolme(int s1, int s2){
+    static int liste[2];
+    int r = s1;
+    int q = 0;
 
-int fac(int n){
-  int f=1;
-  while(n>1){
-    f=f*n;
-    n--;
-  }
-  return f;
+    while( r >= s2 ){
+        int k = 1;
+        int x = s2;
+        int t;
+
+        while( ( t = x+x ) < r ){
+            x = t;
+            k += k;
+        }
+
+    q += k;
+    r -= x;
+    }
+    liste[0]=q;
+    liste[1]=r;
+    return liste;
+}
+int oran(int s1, int s2){
+    int count =0;
+    int *p;
+    int i;
+    for(i=0;i<3;i++){
+        p = bolme(s1,s2);
+        int sonuc = p[0];
+        int kalan = p[1];
+        intyazdir("%d",sonuc);
+        if(i == 0){
+        yazdir(",");
+        }
+        s1 = kalan *10; 
+
+    }
+}
 }
 
 int main(void) {
   hal_setup(CLOCK_FAST);
-
+  
   // marker for automated benchmarks
-  yazdir("Hello World!!!");
-  intyazdir("%d\n",fac(5));
-  yazdir("#");
-
+  oran(14,13);
+  oran(1,2);
+  oran(3,5);
+  oran(8,3);
   while (1);
 
   return 0;
